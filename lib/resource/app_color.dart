1811fe3f6a610376b40d9426d2dbcd51c854221c
leeeeeoy <hoheho18@naver.com>
2022-07-07 21:48:14 +0900

    add: AppColor 세팅

--- lib/resource/app_color.dart
+++ lib/resource/app_color.dart
@@ -1,3 +1,40 @@
 part of 'resource.dart';
 
-abstract class AppColor {}
+abstract class AppColor {
+  static const primary = orange400;
+  static const red = Color(0xffd83232);
+  static const blue = Color(0xff2563eb);
+  static const white = Color(0xffffffff);
+  static const black = Color(0xff000000);
+
+  static const orange50 = Color(0xfffff0e6);
+  static const orange100 = Color(0xfffedcc7);
+  static const orange200 = Color(0xfffdba90);
+  static const orange300 = Color(0xfffd9759);
+  static const orange400 = Color(0xfffc7521);
+  static const orange500 = Color(0xfffc6406);
+  static const orange600 = Color(0xffca5005);
+  static const orange700 = Color(0xff973c04);
+  static const orange800 = Color(0xff652802);
+  static const orange900 = Color(0xff4c1e02);
+
+  static const grey50 = Color(0xfff6f6f6);
+  static const grey100 = Color(0xffefefef);
+  static const grey200 = Color(0xffe3e3e3);
+  static const grey300 = Color(0xffd1d1d1);
+  static const grey400 = Color(0xffacacac);
+  static const grey500 = Color(0xff8b8b8b);
+  static const grey600 = Color(0xff646464);
+  static const grey700 = Color(0xff515151);
+  static const grey800 = Color(0xff333333);
+  static const grey900 = Color(0xff131313);
+
+  static const scarlet50 = Color(0xffffebee);
+  static const scarlet500 = Color(0xfff44336);
+
+  static const amber50 = Color(0xfffff3e0);
+  static const amber500 = Color(0xffff9800);
+
+  static const green50 = Color(0xffdff5e8);
+  static const green500 = Color(0xff26ba6a);
+}
