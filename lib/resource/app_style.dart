4246559e30b18490fb7fb3d21b88435e3b45c1b1
leeeeeoy <hoheho18@naver.com>
2022-07-07 21:58:53 +0900

    add: AppStyle 추가

--- lib/resource/app_style.dart
+++ lib/resource/app_style.dart
@@ -1,3 +1,98 @@
 part of 'resource.dart';
 
-abstract class AppStyle {}
+abstract class AppStyle {
+  static const title1 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
+  );
+
+  static const title2 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+  );
+
+  static const title3Bold = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
+  );
+
+  static const title3Medium = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const subTitle1Bold = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+  );
+
+  static const subTitle1Medium = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const subTitle2Bold = TextStyle(
+    fontSize: 15,
+    fontWeight: FontWeight.w700,
+  );
+
+  static const subTitle2Medium = TextStyle(
+    fontSize: 15,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const subTitle3Bold = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w700,
+  );
+
+  static const subTitle3Medium = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const body1 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const body2 = TextStyle(
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const body3 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const body4 = TextStyle(
+    fontSize: 13,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const body5 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const caption1Bold = TextStyle(
+    fontSize: 13,
+    fontWeight: FontWeight.w700,
+  );
+
+  static const caption1Medium = TextStyle(
+    fontSize: 13,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const caption2Bold = TextStyle(
+    fontSize: 13,
+    fontWeight: FontWeight.w700,
+  );
+
+  static const caption2Medium = TextStyle(
+    fontSize: 13,
+    fontWeight: FontWeight.w500,
+  );
+}
