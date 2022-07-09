dbe64cd09a7c1a052bcee8ffe7e6015b7f63cabd
leeeeeoy <hoheho18@naver.com>
2022-07-09 17:57:05 +0900

    feat: Event model 작성 #6

---
+++ lib/data/model/event/event.dart
@@ -0,0 +1,17 @@
+import 'package:cafein_flutter/cafein_config.dart';
+import 'package:cafein_flutter/data/model/common/image_id_pair.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'event.g.dart';
+part 'event.freezed.dart';
+
+@freezed
+class Event with _$Event {
+  factory Event({
+    required int eventId,
+    required int boardId,
+    @JsonKey(name: 'eventImageDto') required ImageIdPair imageIdPair,
+  }) = _Event;
+
+  factory Event.fromJson(Map<String, dynamic> json) => _$EventFromJson(json);
+}
