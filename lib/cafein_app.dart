c9930814a8ea6a71f872f64efea5b105dd35f851
leeeeeoy <hoheho18@naver.com>
2022-07-10 13:43:10 +0900

    feat: UserRepository 작성 #4

--- lib/cafein_app.dart
+++ lib/cafein_app.dart
@@ -1,8 +1,11 @@
 import 'package:cafein_flutter/cafein_route.dart';
 import 'package:cafein_flutter/data/datasource/local/app_database.dart';
 import 'package:cafein_flutter/data/datasource/remote/dio_util.dart';
+import 'package:cafein_flutter/data/datasource/remote/form_data_client/member_form_data_client.dart';
 import 'package:cafein_flutter/data/datasource/remote/retrofit/auth_client.dart';
+import 'package:cafein_flutter/data/datasource/remote/retrofit/member_client.dart';
 import 'package:cafein_flutter/data/repository/auth_repository.dart';
+import 'package:cafein_flutter/data/repository/user_repository.dart';
 import 'package:cafein_flutter/feature/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -18,6 +21,12 @@ class CafeinApp extends StatelessWidget {
           create: (context) => AuthRepositoryImpl(
             authClient: AuthClient(DioUtil().authDio),
             authPreference: AppDatabase().authPreference,
+          ),
+        ),
+        RepositoryProvider<UserRepository>(
+          create: (context) => UserRepositoryImpl(
+            memberClient: MemberClient(DioUtil().dio),
+            memberFormDataClient: MemberFormDataClient(dio: DioUtil().dio),
           ),
         ),
       ],
---
+++ lib/data/repository/user_repository.dart
@@ -0,0 +1,56 @@
+import 'package:cafein_flutter/data/datasource/remote/base_response.dart';
+import 'package:cafein_flutter/data/datasource/remote/form_data_client/member_form_data_client.dart';
+import 'package:cafein_flutter/data/datasource/remote/retrofit/member_client.dart';
+import 'package:cafein_flutter/data/model/member/member.dart';
+import 'package:cafein_flutter/data/model/member/phone_number_request.dart';
+import 'package:cafein_flutter/data/model/member/update_member_request.dart';
+
+abstract class UserRepository {
+  Member? _member;
+
+  set setMemberData(Member member);
+  get getMemberData => _member;
+
+  Future<BaseResponse<dynamic>> updatePhoneNumber({
+    required int memberId,
+    required PhoneNumberRequest phoneNumberRequest,
+  });
+
+  Future<BaseResponse<Member>> getMember();
+
+  Future<BaseResponse<dynamic>> updateMember(
+    UpdateMemberRequest updateMemberRequest,
+  );
+}
+
+class UserRepositoryImpl extends UserRepository {
+  final MemberClient memberClient;
+  final MemberFormDataClient memberFormDataClient;
+
+  UserRepositoryImpl({
+    required this.memberClient,
+    required this.memberFormDataClient,
+  });
+
+  @override
+  Future<BaseResponse<Member>> getMember() => memberClient.getMember();
+
+  @override
+  Future<BaseResponse> updateMember(
+    UpdateMemberRequest updateMemberRequest,
+  ) =>
+      memberFormDataClient.updateMember(updateMemberRequest);
+
+  @override
+  Future<BaseResponse> updatePhoneNumber({
+    required int memberId,
+    required PhoneNumberRequest phoneNumberRequest,
+  }) =>
+      memberClient.updatePhoneNumber(
+        memberId,
+        phoneNumberRequest,
+      );
+
+  @override
+  set setMemberData(Member member) => _member = member;
+}
