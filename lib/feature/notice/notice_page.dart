db922927b8b67aa9aadce5bb8d8597575b379560
22sonamu <22sonamu@naver.com>
2022-07-14 22:07:26 +0900

    파일 생성 #14

---
+++ lib/feature/notice/notice_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NoticePage extends StatelessWidget {
+  const NoticePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(
+        actions: const [
+
+        ],
+      ),
+
+    );
+  }
+}
