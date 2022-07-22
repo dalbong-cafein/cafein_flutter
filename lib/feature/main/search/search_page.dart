e758a20a958e9e3e3911e0c37f6e7a81d9c6cc15
leeeeeoy <hoheho18@naver.com>
2022-07-22 14:57:15 +0900

    feat: 위치 권한 설정

--- lib/feature/main/search/search_page.dart
+++ lib/feature/main/search/search_page.dart
@@ -1,15 +1,31 @@
+import 'dart:developer';
+
 import 'package:cafein_flutter/feature/main/main_bottom_navigation_bar.dart';
+import 'package:cafein_flutter/widget/dialog/permission_dialog.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class SearchPage extends StatelessWidget {
   const SearchPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      bottomNavigationBar: MainBottomNavigationBar(),
+    return Scaffold(
+      bottomNavigationBar: const MainBottomNavigationBar(),
       body: Center(
-        child: Text('SearchPage'),
+        child: ElevatedButton(
+          onPressed: () async {
+            final status = await Permission.locationWhenInUse.request();
+            if (!status.isGranted) {
+              final result = await PermissionDialog.show(context);
+              if (result) {
+                openAppSettings();
+              }
+            }
+            log('$status');
+          },
+          child: const Text('위치권한'),
+        ),
       ),
     );
   }
