b3e1aad23a234d278e5203ffc028d3b5aa876383
22sonamu <22sonamu@naver.com>
2022-08-06 15:51:14 +0900

    fix : 메인 화면 원형 프로필 에러 수정 #18

--- lib/feature/main/home/home_page.dart
+++ lib/feature/main/home/home_page.dart
@@ -50,20 +50,20 @@ class HomePage extends StatelessWidget {
                 padding: const EdgeInsets.only(
                   right: 24,
                 ),
-                child: CircleAvatar(
-                  radius: 32,
-                  child: memberData?.imageIdPair?.imageUrl == null
-                      ? loadAsset(
-                          CafeinConst.defaultProfiles[Random().nextInt(2)],
-                          width: 32,
-                          height: 32,
-                        )
-                      : Image.network(
-                          memberData!.imageIdPair!.imageUrl!,
-                          width: 32,
-                          height: 32,
-                        ),
-                ),
+                child:SizedBox(
+                  width: 32,
+                  height: 32,
+                  child: memberData?.imageIdPair?.imageUrl == null ? CircleAvatar(
+                    child: loadAsset(
+                      CafeinConst.defaultProfiles[Random().nextInt(2)],
+                      width: 32,
+                      height: 32,
+                    )
+                  ) : CircleAvatar(
+                    radius: 32,
+                    backgroundImage: NetworkImage(memberData?.imageIdPair?.imageUrl ?? 'url'),
+                  ),
+                )
               ),
             ],
           ),
