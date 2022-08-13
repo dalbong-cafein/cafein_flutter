f8987c1d8f7e17d6a5dffd8ce4dc7afbc1be3126
22sonamu <22sonamu@naver.com>
2022-08-13 12:39:29 +0900

    feat : 스티커 bloc 작성 #24

--- lib/feature/sticker/bloc/sticker_bloc.dart
+++ lib/feature/sticker/bloc/sticker_bloc.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:cafein_flutter/data/model/sticker/sticker.dart';
+import 'package:cafein_flutter/data/repository/sticker_repository.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
@@ -9,9 +10,30 @@ part 'sticker_event.dart';
 part 'sticker_state.dart';
 
 class StickerBloc extends Bloc<StickerEvent, StickerState> {
-  StickerBloc() : super(StickerInitial()) {
-    on<StickerEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  StickerBloc({required this.stickerRepository}) : super(const StickerInitial()) {
+    on<StickerRequested>(_onStickerRequested);
+  }
+  final StickerRepository stickerRepository;
+
+  FutureOr<void> _onStickerRequested(
+      StickerRequested event,
+      Emitter<StickerState> emit,
+      ) async {
+      emit(StickerLoading());
+      try{
+        final stickerCntResponse = await stickerRepository.getStickerCount();
+        final stickersResponse = await stickerRepository.getStickers();
+        final stickerCnt = stickerCntResponse.data;
+        final stickers = stickersResponse.data;
+        emit(StickerLoaded(
+            stickerCnt: stickerCnt,
+            stickers: stickers
+        ));
+      }catch(e){
+        emit(StickerError(
+          error: e,
+          event: () => add(event),
+        ));
+      }
   }
 }
