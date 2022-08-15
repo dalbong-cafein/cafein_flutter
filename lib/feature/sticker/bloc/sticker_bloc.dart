3cb90f64129b6904b6e4deab90200afab3491b05
22sonamu <22sonamu@naver.com>
2022-08-15 23:43:28 +0900

    feat : 쿠폰 갯수 확인 bloc 작성 #24

--- lib/feature/sticker/bloc/sticker_bloc.dart
+++ lib/feature/sticker/bloc/sticker_bloc.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:cafein_flutter/data/model/sticker/sticker.dart';
+import 'package:cafein_flutter/data/repository/coupon_repository.dart';
 import 'package:cafein_flutter/data/repository/sticker_repository.dart';
 import 'package:equatable/equatable.dart';
 
@@ -9,10 +10,12 @@ part 'sticker_event.dart';
 part 'sticker_state.dart';
 
 class StickerBloc extends Bloc<StickerEvent, StickerState> {
-  StickerBloc({required this.stickerRepository}) : super(const StickerInitial()) {
+  StickerBloc({required this.stickerRepository, required this.couponRepository}) : super(const StickerInitial()) {
     on<StickerRequested>(_onStickerRequested);
+    on<CouponCountRequested>(_onCouponCountRequested);
   }
   final StickerRepository stickerRepository;
+  final CouponRepository couponRepository;
 
   FutureOr<void> _onStickerRequested(
       StickerRequested event,
@@ -34,5 +37,23 @@ class StickerBloc extends Bloc<StickerEvent, StickerState> {
           event: () => add(event),
         ));
       }
+  }
+  FutureOr<void> _onCouponCountRequested(
+      CouponCountRequested event,
+      Emitter<StickerState> emit,
+      ) async {
+    emit(StickerLoading());
+    try{
+
+      final couponResponse = await couponRepository.getCoupons();
+      final int couponCnt = couponResponse.data.length;
+      emit(CouponCountLoaded(couponCnt: couponCnt));
+
+    }catch(e){
+      emit(StickerError(
+        error: e,
+        event: () => add(event),
+      ));
+    }
   }
 }
