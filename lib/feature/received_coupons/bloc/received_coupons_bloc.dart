58f278115ffd8fe3c7b1c49dfcb1d6d71f10b4e6
22sonamu <22sonamu@naver.com>
2022-08-21 02:38:15 +0900

    feat : 받은 쿠폰 bloc 작성 #29

--- lib/feature/received_coupons/bloc/received_coupons_bloc.dart
+++ lib/feature/received_coupons/bloc/received_coupons_bloc.dart
@@ -2,16 +2,35 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:cafein_flutter/data/model/coupon/coupon.dart';
+import 'package:cafein_flutter/data/repository/coupon_repository.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
 part 'received_coupons_event.dart';
+
 part 'received_coupons_state.dart';
 
-class ReceivedCouponsBloc extends Bloc<ReceivedCouponsEvent, ReceivedCouponsState> {
-  ReceivedCouponsBloc() : super(ReceivedCouponsInitial()) {
-    on<ReceivedCouponsEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+class ReceivedCouponsBloc
+    extends Bloc<ReceivedCouponsEvent, ReceivedCouponsState> {
+  ReceivedCouponsBloc({required this.couponRepository}) : super(const ReceivedCouponsInitial()) {
+    on<ReceivedCouponsRequested>(_onReceivedCouponsRequested);
+  }
+
+  final CouponRepository couponRepository;
+
+  FutureOr<void> _onReceivedCouponsRequested(
+      ReceivedCouponsRequested event,
+      Emitter<ReceivedCouponsState> emit) async {
+    emit(const ReceivedCouponsLoading());
+    try {
+      final couponResponse = await couponRepository.getCoupons();
+      final coupons = couponResponse.data;
+      emit(ReceivedCouponsLoaded(coupons: [...coupons]));
+    } catch (e) {
+      emit(ReceivedCouponsError(
+        error: e,
+        event: () => add(event),
+      ));
+    }
   }
 }
