936f06bf3a3d13611c42b552b4e37538a25ea304
22sonamu <22sonamu@naver.com>
2022-08-27 14:59:06 +0900

    refactor : apply coupon blocbuilder 위치 변경 #29

--- lib/feature/apply_coupon/apply_coupon_page.dart
+++ lib/feature/apply_coupon/apply_coupon_page.dart
@@ -12,187 +12,207 @@ class ApplyCouponPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     context.read<ApplyCouponBloc>().add(CouponInitialLoading());
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "쿠폰 신청",
-          style: AppStyle.subTitle16Medium,
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: BlocBuilder<ApplyCouponBloc, ApplyCouponState>(
-          buildWhen: (pre, next) => next is CouponClickLoaded,
-          builder: (context, state) {
-            if(state is CouponClickLoaded){
-              return GridView.builder(
-                  physics: const NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  itemCount: 5,
-                  gridDelegate:
-                  const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    childAspectRatio: 158 / 240,
-                    mainAxisSpacing: 12,
-                    crossAxisSpacing: 12,
-                  ),
-                  itemBuilder: (BuildContext context, int index) {
-                    return Padding(
-                      padding: index % 2 == 0 ?
-                      const EdgeInsets.only(left: 16) : const EdgeInsets.only(
-                          right: 16),
-                      child: InkWell(
-                        onTap: (){
-                          context.read<ApplyCouponBloc>().add(CouponClicked(clickedIndex: index));
-                        },
-                        child: Container(
-                          height: 240,
-                          decoration: BoxDecoration(
-                            color: AppColor.white,
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey.withOpacity(0.1),
-                                spreadRadius: 4,
-                                blurRadius: 4,
-                              ),
-                            ],
-                            border: Border.all(
-                                width: 1,
-                                color : index == state.clickedIndex ? AppColor.orange500 : Colors.transparent
-                            ),
-                            borderRadius: const BorderRadius.all(
-                                Radius.circular(10.0)
-                            ),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.only(
-                                left: 12, right: 12, top: 12, bottom: 16),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                loadAsset(
-                                    CafeinConst.couponStoreImages[index]
-                                ),
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                Row(
-                                  children: [
-                                    loadAsset(
-                                        CafeinConst.couponStoreIcons[index],
-                                        height: 16,
-                                        width: 16
-                                    ),
-                                    const SizedBox(
-                                      width: 4,
-                                    ),
-                                    Text(
-                                      CafeinConst.couponStoreNames[index],
-                                      style: AppStyle.caption12Regular.copyWith(
-                                          color: AppColor.grey600
-                                      ),
-                                    )
-                                  ],
-                                ),
-                                const SizedBox(
-                                  height: 8,
-                                ),
-                                Text(
-                                  CafeinConst.couponItemNames[index],
-                                  style: AppStyle.subTitle15Bold,
-                                )
+    return BlocConsumer<ApplyCouponBloc, ApplyCouponState>(
+      listener: (context, state) {
+        // TODO: implement listener
+      },
+      builder: (context, state) {
+        if(state is CouponClickLoaded){
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text(
+                "쿠폰 신청",
+                style: AppStyle.subTitle16Medium,
+              ),
+            ),
 
+            body: SingleChildScrollView(
+                child: GridView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: 5,
+                    gridDelegate:
+                    const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 158 / 240,
+                      mainAxisSpacing: 12,
+                      crossAxisSpacing: 12,
+                    ),
+                    itemBuilder: (BuildContext context, int index) {
+                      return Padding(
+                        padding: index % 2 == 0 ?
+                        const EdgeInsets.only(left: 16) : const EdgeInsets.only(
+                            right: 16),
+                        child: InkWell(
+                          onTap: () {
+                            context.read<ApplyCouponBloc>().add(CouponClicked(
+                                clickedIndex: index));
+                          },
+                          child: Container(
+                            height: 240,
+                            decoration: BoxDecoration(
+                              color: AppColor.white,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.1),
+                                  spreadRadius: 4,
+                                  blurRadius: 4,
+                                ),
                               ],
+                              border: Border.all(
+                                  width: 2,
+                                  color: index == state.clickedIndex ? AppColor
+                                      .orange500 : Colors.transparent
+                              ),
+                              borderRadius: const BorderRadius.all(
+                                  Radius.circular(10.0)
+                              ),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 12, right: 12, top: 12, bottom: 16),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  loadAsset(
+                                      CafeinConst.couponStoreImages[index]
+                                  ),
+                                  const SizedBox(
+                                    height: 10,
+                                  ),
+                                  Row(
+                                    children: [
+                                      loadAsset(
+                                          CafeinConst.couponStoreIcons[index],
+                                          height: 16,
+                                          width: 16
+                                      ),
+                                      const SizedBox(
+                                        width: 4,
+                                      ),
+                                      Text(
+                                        CafeinConst.couponStoreNames[index],
+                                        style: AppStyle.caption12Regular.copyWith(
+                                            color: AppColor.grey600
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                  const SizedBox(
+                                    height: 8,
+                                  ),
+                                  Text(
+                                    CafeinConst.couponItemNames[index],
+                                    style: AppStyle.subTitle15Bold,
+                                  )
+
+                                ],
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    );
-                  });
-            }else{
-              return GridView.builder(
-                  physics: const NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  itemCount: 5,
-                  gridDelegate:
-                  const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    childAspectRatio: 158 / 240,
-                    mainAxisSpacing: 12,
-                    crossAxisSpacing: 12,
-                  ),
-                  itemBuilder: (BuildContext context, int index) {
-                    return Padding(
-                      padding: index % 2 == 0 ?
-                      const EdgeInsets.only(left: 16) : const EdgeInsets.only(
-                          right: 16),
-                      child: InkWell(
-                        onTap: (){
-                          context.read<ApplyCouponBloc>().add(CouponClicked(clickedIndex: index));
-                        },
-                        child: Container(
-                          height: 240,
-                          decoration: BoxDecoration(
-                            color: AppColor.white,
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey.withOpacity(0.1),
-                                spreadRadius: 4,
-                                blurRadius: 4,
-                              ),
-                            ],
-                            borderRadius: const BorderRadius.all(
-                                Radius.circular(10.0)
-                            ),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.only(
-                                left: 12, right: 12, top: 12, bottom: 16),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                loadAsset(
-                                    CafeinConst.couponStoreImages[index]
-                                ),
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                Row(
-                                  children: [
-                                    loadAsset(
-                                        CafeinConst.couponStoreIcons[index],
-                                        height: 16,
-                                        width: 16
-                                    ),
-                                    const SizedBox(
-                                      width: 4,
-                                    ),
-                                    Text(
-                                      CafeinConst.couponStoreNames[index],
-                                      style: AppStyle.caption12Regular.copyWith(
-                                          color: AppColor.grey600
-                                      ),
-                                    )
-                                  ],
-                                ),
-                                const SizedBox(
-                                  height: 8,
-                                ),
-                                Text(
-                                  CafeinConst.couponItemNames[index],
-                                  style: AppStyle.subTitle15Bold,
-                                )
+                      );
+                    })
+            ),
+          );
 
+        }else{
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text(
+                "쿠폰 신청",
+                style: AppStyle.subTitle16Medium,
+              ),
+            ),
+
+            body: SingleChildScrollView(
+                child: GridView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: 5,
+                    gridDelegate:
+                    const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 158 / 240,
+                      mainAxisSpacing: 12,
+                      crossAxisSpacing: 12,
+                    ),
+                    itemBuilder: (BuildContext context, int index) {
+                      return Padding(
+                        padding: index % 2 == 0 ?
+                        const EdgeInsets.only(left: 16) : const EdgeInsets.only(
+                            right: 16),
+                        child: InkWell(
+                          onTap: () {
+                            context.read<ApplyCouponBloc>().add(CouponClicked(
+                                clickedIndex: index));
+                          },
+                          child: Container(
+                            height: 240,
+                            decoration: BoxDecoration(
+                              color: AppColor.white,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.1),
+                                  spreadRadius: 4,
+                                  blurRadius: 4,
+                                ),
                               ],
+
+                              borderRadius: const BorderRadius.all(
+                                  Radius.circular(10.0)
+                              ),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 12, right: 12, top: 12, bottom: 16),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  loadAsset(
+                                      CafeinConst.couponStoreImages[index]
+                                  ),
+                                  const SizedBox(
+                                    height: 10,
+                                  ),
+                                  Row(
+                                    children: [
+                                      loadAsset(
+                                          CafeinConst.couponStoreIcons[index],
+                                          height: 16,
+                                          width: 16
+                                      ),
+                                      const SizedBox(
+                                        width: 4,
+                                      ),
+                                      Text(
+                                        CafeinConst.couponStoreNames[index],
+                                        style: AppStyle.caption12Regular.copyWith(
+                                            color: AppColor.grey600
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                  const SizedBox(
+                                    height: 8,
+                                  ),
+                                  Text(
+                                    CafeinConst.couponItemNames[index],
+                                    style: AppStyle.subTitle15Bold,
+                                  )
+
+                                ],
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    );
-                  });
-            }
-          },
-        ),
-      ),
+                      );
+                    })
+            ),
+          );
+        }
+
+      },
     );
   }
 }
