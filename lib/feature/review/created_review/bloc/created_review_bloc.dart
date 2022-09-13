0f95a276920265c1e4c5b5bdddda7a98f7ba45b8
22sonamu <22sonamu@naver.com>
2022-09-14 00:14:51 +0900

    feat: createdReview bloc 작성 #32

--- lib/feature/review/created_review/bloc/created_review_bloc.dart
+++ lib/feature/review/created_review/bloc/created_review_bloc.dart
@@ -7,12 +7,24 @@ import 'package:meta/meta.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 part 'created_review_event.dart';
+
 part 'created_review_state.dart';
 
 class CreatedReviewBloc extends Bloc<CreatedReviewEvent, CreatedReviewState> {
   CreatedReviewBloc() : super(CreatedReviewInitial()) {
-    on<CreatedReviewEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<CreatedReviewPermissionRequested>(_onCreatedReviewPermissionRequested);
+  }
+
+  FutureOr<void> _onCreatedReviewPermissionRequested(
+    CreatedReviewPermissionRequested event,
+    Emitter<CreatedReviewState> emit,
+  ) async {
+    final status = await event.permission.request();
+    emit(
+      CreatedReviewPermissionChecked(
+        permission: event.permission,
+        permissionStatus: status,
+      ),
+    );
   }
 }
