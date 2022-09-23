dfcc40c4cab667cfd055fddb28f9ca6cc9277d8c
22sonamu <22sonamu@naver.com>
2022-09-23 22:00:19 +0900

    feat : 신고 안내 문구 작성 #37

--- lib/feature/report/report_page.dart
+++ lib/feature/report/report_page.dart
@@ -1,4 +1,5 @@
 import 'package:cafein_flutter/resource/resource.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ReportPage extends StatelessWidget {
@@ -8,11 +9,30 @@ class ReportPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          "스티커",
-          style: AppStyle.subTitle16Medium,
-        ),
       ),
+      body : Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24,
+          vertical: 24
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "신고하는 이유를 알려주세요",
+              style: AppStyle.title21Bold.copyWith(
+                color : AppColor.grey900
+              ),
+            ),
+            Text(
+              "신고 이유가 타당하지 않으면 반영되지 않을 수\n있습니다.",
+              style: AppStyle.body14Regular.copyWith(
+                color : AppColor.grey600
+              ),
+            )
+          ],
+        ),
+      )
     );
   }
 }
