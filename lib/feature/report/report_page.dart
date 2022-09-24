9aaabeea6b61519996a430f2ef1e8f69032aaee6
22sonamu <22sonamu@naver.com>
2022-09-24 23:05:05 +0900

    feat : bottomsheet 작성 #37

--- lib/feature/report/report_page.dart
+++ lib/feature/report/report_page.dart
@@ -1,6 +1,5 @@
 import 'package:cafein_flutter/resource/resource.dart';
 import 'package:cafein_flutter/util/load_asset.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ReportPage extends StatelessWidget {
@@ -11,65 +10,85 @@ class ReportPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
     return Scaffold(
-        appBar: AppBar(),
-        body: Padding(
-          padding: const EdgeInsets.only(top: 24, left: 24, right: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "신고하는 이유를 알려주세요",
-                style: AppStyle.title21Bold.copyWith(color: AppColor.grey900),
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              Text(
-                "신고 이유가 타당하지 않으면 반영되지 않을 수\n있습니다.",
-                style: AppStyle.body14Regular.copyWith(color: AppColor.grey600),
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-              ListView.builder(
-                  itemCount: 1,
-                  physics: const NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  itemBuilder: (BuildContext context, int index) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 16
-                  ),
-                  child: Row(
-                    children: [
-                      SizedBox(
-                        width: (width - 44) / 2,
-                        child : const Text(
-                          "gg",
-                          style: AppStyle.subTitle15Medium
-                        )
-                      ),
-                      SizedBox(
-                        width: (width - 44) / 2,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end
-                          ,children: [
-                            loadAsset(
-                              AppIcon.doneOff,
-                              fit: BoxFit.scaleDown,
-                              height: 24,
-                              width: 24
-                            ),
-                          ],
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 24, left: 24, right: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "신고하는 이유를 알려주세요",
+              style: AppStyle.title21Bold.copyWith(color: AppColor.grey900),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            Text(
+              "신고 이유가 타당하지 않으면 반영되지 않을 수\n있습니다.",
+              style: AppStyle.body14Regular.copyWith(color: AppColor.grey600),
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            ListView.builder(
+                itemCount: 1,
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    child: Row(
+                      children: [
+                        SizedBox(
+                            width: (width - 44) / 2,
+                            child: const Text("gg",
+                                style: AppStyle.subTitle15Medium)),
+                        SizedBox(
+                          width: (width - 44) / 2,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              loadAsset(AppIcon.doneOff,
+                                  fit: BoxFit.scaleDown, height: 24, width: 24),
+                            ],
+                          ),
                         ),
-                      ),
-
-                    ],
+                      ],
+                    ),
+                  );
+                })
+          ],
+        ),
+      ),
+      bottomSheet: SizedBox(
+        height: 56,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 9),
+              child: Container(
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(14.0)),
+                  color: AppColor.orange100,
+                ),
+                width: width - 32,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  child: Center(
+                    child: Text(
+                      "신고하기",
+                      style: AppStyle.subTitle15Medium
+                          .copyWith(color: AppColor.white),
+                    ),
                   ),
-                );
-              })
-            ],
-          ),
-        ));
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
