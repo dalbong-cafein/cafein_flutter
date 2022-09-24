11d850bc6f82fd2d1c4c61ce7e1e214e4e8d0e74
22sonamu <22sonamu@naver.com>
2022-09-24 23:26:18 +0900

    feat : blocconsumer 와 Error처리 작성 #37

--- lib/feature/report/report_page.dart
+++ lib/feature/report/report_page.dart
@@ -1,6 +1,9 @@
+import 'package:cafein_flutter/feature/report/bloc/report_bloc.dart';
 import 'package:cafein_flutter/resource/resource.dart';
 import 'package:cafein_flutter/util/load_asset.dart';
+import 'package:cafein_flutter/widget/dialog/error_dialog.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ReportPage extends StatelessWidget {
   const ReportPage({Key? key}) : super(key: key);
@@ -11,54 +14,70 @@ class ReportPage extends StatelessWidget {
     final width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 24, left: 24, right: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "신고하는 이유를 알려주세요",
-              style: AppStyle.title21Bold.copyWith(color: AppColor.grey900),
-            ),
-            const SizedBox(
-              height: 12,
-            ),
-            Text(
-              "신고 이유가 타당하지 않으면 반영되지 않을 수\n있습니다.",
-              style: AppStyle.body14Regular.copyWith(color: AppColor.grey600),
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            ListView.builder(
-                itemCount: 1,
-                physics: const NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                itemBuilder: (BuildContext context, int index) {
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 16),
-                    child: Row(
-                      children: [
-                        SizedBox(
-                            width: (width - 44) / 2,
-                            child: const Text("gg",
-                                style: AppStyle.subTitle15Medium)),
-                        SizedBox(
-                          width: (width - 44) / 2,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              loadAsset(AppIcon.doneOff,
-                                  fit: BoxFit.scaleDown, height: 24, width: 24),
-                            ],
-                          ),
+      body: BlocConsumer<ReportBloc, ReportState>(
+        listener: (context, state) {
+          if (state is ReportError) {
+            ErrorDialog.show(
+              context,
+              error: state.error,
+              refresh: state.event,
+            );
+          }
+        },
+        builder: (context, state) {
+          return Padding(
+            padding: const EdgeInsets.only(top: 24, left: 24, right: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "신고하는 이유를 알려주세요",
+                  style: AppStyle.title21Bold.copyWith(color: AppColor.grey900),
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                Text(
+                  "신고 이유가 타당하지 않으면 반영되지 않을 수\n있습니다.",
+                  style:
+                      AppStyle.body14Regular.copyWith(color: AppColor.grey600),
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                ListView.builder(
+                    itemCount: 1,
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 16),
+                        child: Row(
+                          children: [
+                            SizedBox(
+                                width: (width - 44) / 2,
+                                child: const Text("gg",
+                                    style: AppStyle.subTitle15Medium)),
+                            SizedBox(
+                              width: (width - 44) / 2,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  loadAsset(AppIcon.doneOff,
+                                      fit: BoxFit.scaleDown,
+                                      height: 24,
+                                      width: 24),
+                                ],
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                  );
-                })
-          ],
-        ),
+                      );
+                    })
+              ],
+            ),
+          );
+        },
       ),
       bottomSheet: SizedBox(
         height: 56,
