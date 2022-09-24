d6b4c6de1183eb277fa99c580a1965192e29ae1e
22sonamu <22sonamu@naver.com>
2022-09-24 23:22:34 +0900

    feat : bloc 작성 #37

--- lib/feature/report/bloc/report_bloc.dart
+++ lib/feature/report/bloc/report_bloc.dart
@@ -2,25 +2,30 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:cafein_flutter/data/model/report/report_category.dart';
+import 'package:cafein_flutter/data/repository/review_repository.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/services.dart';
 
 part 'report_event.dart';
+
 part 'report_state.dart';
 
 class ReportBloc extends Bloc<ReportEvent, ReportState> {
-  ReportBloc() : super(ReportInitial()) {
-    on<ReportEvent>((event, emit) {
-    });
+  ReportBloc({required this.reviewRepository}) : super(ReportInitial()) {
+    on<ReportCategoryRequested>(_onReportCategoryRequested);
   }
 
+  final ReviewRepository reviewRepository;
+
   FutureOr<void> _onReportCategoryRequested(
-      ReportCategoryRequested event,
-      Emitter<ReportState> emit,
-      ) async {
+    ReportCategoryRequested event,
+    Emitter<ReportState> emit,
+  ) async {
     emit(const ReportLoading());
     try {
-
+      final categoryResponse = await reviewRepository.getReportCategories();
+      final categories = categoryResponse.data;
+      emit(ReportCategoryLoaded(categories: [...categories]));
     } catch (e) {
       emit(ReportError(
         error: e,
