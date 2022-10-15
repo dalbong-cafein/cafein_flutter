53e6a3039413e559f629f42a73a2b78dd3cdbc32
22sonamu <22sonamu@naver.com>
2022-10-15 15:53:33 +0900

    수정 : 스티커 20개 이상일 시 쿠폰 신청 버튼 노출 #49

--- lib/feature/sticker/sticker_page.dart
+++ lib/feature/sticker/sticker_page.dart
@@ -114,7 +114,7 @@ class StickerPage extends StatelessWidget {
               },
               builder: (context, state) {
                 if (state is StickerLoaded) {
-                  if (state.couponCnt > 0) {
+                  if (state.couponCnt >= 20) {
                     return InkWell(
                         onTap: () {
                           Navigator.of(context).pushNamed(ReceivedCouponsPage.routeName);
