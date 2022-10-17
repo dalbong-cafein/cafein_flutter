2a4e6f41db6de6892a6dea6057ac1a69e024af4b
leeeeeoy <hoheho18@naver.com>
2022-10-17 20:19:57 +0900

    feat: FavoriteStorePage route 세팅

---
+++ lib/feature/store/favorite_store/favorite_store_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class FavoriteStorePage extends StatelessWidget {
+  const FavoriteStorePage({Key? key}) : super(key: key);
+
+  static const routeName = 'FavoriteStorePage';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
