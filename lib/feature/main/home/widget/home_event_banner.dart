063197085d5046bb113f678b9ffe063dcbd67224
22sonamu <22sonamu@naver.com>
2022-10-31 00:12:07 +0900

    feat : 홈 이벤트 이미지 연결 #58

--- lib/feature/main/home/widget/home_event_banner.dart
+++ lib/feature/main/home/widget/home_event_banner.dart
@@ -1,12 +1,33 @@
+import 'package:cafein_flutter/feature/main/home/bloc/home_bloc.dart';
 import 'package:cafein_flutter/resource/resource.dart';
 import 'package:cafein_flutter/util/load_asset.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeEventBanner extends StatelessWidget {
   const HomeEventBanner({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return loadAsset(AppImage.eventBanner);
+    final width = MediaQuery.of(context).size.width;
+    return BlocConsumer<HomeBloc, HomeState>(
+      buildWhen: (previous, current) => current is HomeLoaded,
+      listener: (context, state) {
+
+      },
+      builder: (context, state) {
+        if(state is HomeLoaded){
+          return SizedBox(
+            width: width - 32,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20), // Image border
+              child: Image.network(state.homeEventImageUrl),
+            ),
+          );
+        }else{
+          return const SizedBox.shrink();
+        }
+      },
+    );
   }
 }
