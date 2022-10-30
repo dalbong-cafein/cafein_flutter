399ceec714388496e68fba69c06061bdbccf270d
22sonamu <22sonamu@naver.com>
2022-10-30 19:10:51 +0900

    feat : 이벤트 배너 파일 #58

---
+++ lib/feature/main/home/widget/event_banner.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class EventBanner extends StatelessWidget {
+  const EventBanner({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
